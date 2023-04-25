0fd83f96637bcc105599e1ed94e05899a7b2b0b2
Saurabh Suchak <saurabhsuchak1@gmail.com>
2023-04-25 20:18:19 +0530

    updated

--- client/src/models/Department.js
+++ client/src/models/Department.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 
 const departmentSchema = new mongoose.Schema({
+
+    address:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    depmail:{
+        type:String,
+        required:true
+    },
+    deppin:{
+        type:Number,
+        required:true
+    }
+
+
 //   address: String,
 //   govtype: String,
 //   name: String,
 //   govmail: String,
 //   govpin: Number,
+
 });
 
 const Government = mongoose.model('Department', departmentSchema);
